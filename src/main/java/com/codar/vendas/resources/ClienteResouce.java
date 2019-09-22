6380b4b41b6d018875659e016b28ccf67192ac32
Igor Antony <igor.prohmann@gmail.com>
2019-09-22 15:35:17 -0300

    Implementado PUT, GET, Paginação e DELETE para Cliente

--- src/main/java/com/codar/vendas/resources/ClienteResouce.java
+++ src/main/java/com/codar/vendas/resources/ClienteResouce.java
@@ -1,13 +1,24 @@
 package com.codar.vendas.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codar.vendas.domain.Cliente;
+import com.codar.vendas.dto.ClienteDTO;
 import com.codar.vendas.services.ClienteService;
 
 
@@ -23,5 +34,39 @@ public class ClienteResouce {
 		Cliente cliente = clienteService.obter(id);
 		
 		return ResponseEntity.ok().body(cliente);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> atualizar(@Valid @RequestBody ClienteDTO clienteDto, @PathVariable("id") Integer id) {
+		Cliente cliente = clienteService.fromDTO(clienteDto); 
+		cliente.setId(id);
+		cliente = clienteService.atualizar(cliente);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deletar(@PathVariable("id") Integer id) {
+		clienteService.deletar(id);
+		
+		return ResponseEntity.noContent().build();
+	}
+
+	@GetMapping()
+	public ResponseEntity<List<ClienteDTO>> listar() {
+		List<Cliente> clientes = clienteService.listar();
+		List<ClienteDTO> clientesDTO = clientes.stream().map(cliente -> new ClienteDTO(cliente)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(clientesDTO);
+	}
+	
+	@GetMapping("/page")
+	public ResponseEntity<Page<ClienteDTO>> listarPagina(
+			@RequestParam(value = "page", defaultValue = "0") Integer page, 
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
+			@RequestParam(value = "ordeBy", defaultValue = "nome") String orderBy, 
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+		
+		Page<Cliente> clientes = clienteService.listarPorPagina(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> clientesDTO = clientes.map(cliente -> new ClienteDTO(cliente));
+		return ResponseEntity.ok().body(clientesDTO);
 	}
 }
--- src/main/java/com/codar/vendas/services/ClienteService.java
+++ src/main/java/com/codar/vendas/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.codar.vendas.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.codar.vendas.domain.Cliente;
+import com.codar.vendas.dto.ClienteDTO;
 import com.codar.vendas.repository.ClienteRepository;
+import com.codar.vendas.services.exceptions.DataIntegrityException;
 import com.codar.vendas.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -18,5 +25,39 @@ public class ClienteService {
 	public Cliente obter(Integer id) {
 		Optional<Cliente> cliente = clienteRepository.findById(id);
 		return cliente.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id:" + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	public Cliente atualizar(Cliente cliente) {
+		Cliente newCliente = obter(cliente.getId());
+		updateData(newCliente, cliente);
+		return clienteRepository.save(newCliente);
+	}
+	
+	public void deletar(Integer id) {
+		obter(id);
+		try {
+			clienteRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir um cliente que possui pedido e ou endereço.");
+		}
+	}
+	
+	public List<Cliente> listar() {
+		List<Cliente> clientes = clienteRepository.findAll();
+		return clientes;
+	}
+	
+	public Page<Cliente> listarPorPagina(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return clienteRepository.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO clienteDto) {
+		return new Cliente(clienteDto.getId(), clienteDto.getNome(), clienteDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newCliente, Cliente cliente) {
+		newCliente.setNome(cliente.getNome());
+		newCliente.setEmail(cliente.getEmail());
 	}
 }

31dbdcaaee6f36c1cdd1f199b3349cd53fd74c54
Igor Antony <igor.prohmann@gmail.com>
2019-09-22 17:39:28 -0300

    Validação customizada: garantir que email não seja inserido se já estiver na base

--- src/main/java/com/codar/vendas/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/codar/vendas/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.codar.vendas.domain.Cliente;
 import com.codar.vendas.domain.enums.TipoCliente;
 import com.codar.vendas.dto.ClienteNewDTO;
+import com.codar.vendas.repository.ClienteRepository;
 import com.codar.vendas.resources.exceptions.FieldMessage;
 import com.codar.vendas.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -27,6 +34,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCodigo()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente cliente = clienteRepository.findByEmail(objDto.getEmail());
+		
+		if (cliente != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 
 		for (FieldMessage e : list) {

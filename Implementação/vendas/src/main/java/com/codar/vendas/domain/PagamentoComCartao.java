ed0d731faf0b0a20a46554954e515cf86133f32f
Igor Antony <igor.prohmann@gmail.com>
2019-09-25 21:35:56 -0300

    Organizando o Projeto

--- Implementação/vendas/src/main/java/com/codar/vendas/domain/PagamentoComCartao.java
+++ Implementação/vendas/src/main/java/com/codar/vendas/domain/PagamentoComCartao.java
@@ -3,8 +3,10 @@ package com.codar.vendas.domain;
 import javax.persistence.Entity;
 
 import com.codar.vendas.domain.enums.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 

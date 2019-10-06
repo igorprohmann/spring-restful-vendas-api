2483b67e41fc2a35a7019deb124ca461b8d48aca
Igor Antony <igor.prohmann@gmail.com>
2019-10-06 17:56:11 -0300

    Implementando o Recuperar Senha

--- src/main/java/com/codar/vendas/resources/AuthResource.java
+++ src/main/java/com/codar/vendas/resources/AuthResource.java
@@ -1,16 +1,19 @@
 package com.codar.vendas.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codar.vendas.dto.EmailDTO;
 import com.codar.vendas.security.JWTUtil;
 import com.codar.vendas.security.UserSS;
+import com.codar.vendas.services.AuthService;
 import com.codar.vendas.services.UserService;
 
 @RestController
@@ -19,12 +22,21 @@ public class AuthResource {
 	
 	@Autowired
 	private JWTUtil jwtUtil;
+	
+	@Autowired
+	private AuthService authService;
 
 	@PostMapping(value = "/refresh_token")
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PostMapping(value = "/forgot")
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO emailDto) {
+		authService.sendNewPassword(emailDto.getEmail());
 		return ResponseEntity.noContent().build();
 	}
 
--- src/main/java/com/codar/vendas/services/AbstractEmailService.java
+++ src/main/java/com/codar/vendas/services/AbstractEmailService.java
@@ -13,7 +13,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
-import com.codar.vendas.domain.ItemPedido;
+import com.codar.vendas.domain.Cliente;
 import com.codar.vendas.domain.Pedido;
 
 public abstract class AbstractEmailService implements EmailService {
@@ -73,5 +73,21 @@ public abstract class AbstractEmailService implements EmailService {
 		mmh.setText(htmlFromTemplatePedido(pedido), true);
 		return mm;
 	}
+	
+	public void sendNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = prepareNewPasswordEmail(cliente, newPass);
+		sendEmail(sm);		
+	}
+
+	protected SimpleMailMessage prepareNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = new SimpleMailMessage();
+		sm.setTo(cliente.getEmail());
+		sm.setFrom(sender);
+		sm.setSubject("Solicitação de nova senha ");
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		sm.setText("Nova senha: " + newPass);
+		return sm;
+	}
+	
 	//Fim Enviar email HTML
 }

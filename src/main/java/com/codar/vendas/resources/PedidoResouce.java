2010944c95d3756b83988a810d5e3826fca5c764
Igor Antony <igor.prohmann@gmail.com>
2019-10-06 16:43:46 -0300

    Listando pedidos do Cliente

--- src/main/java/com/codar/vendas/resources/PedidoResouce.java
+++ src/main/java/com/codar/vendas/resources/PedidoResouce.java
@@ -5,18 +5,18 @@ import java.net.URI;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.codar.vendas.domain.Categoria;
 import com.codar.vendas.domain.Pedido;
-import com.codar.vendas.dto.CategoriaDTO;
 import com.codar.vendas.services.PedidoService;
 
 
@@ -39,5 +39,15 @@ public class PedidoResouce {
 		pedido = pedidoService.inserir(pedido);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(pedido.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@GetMapping("/page")
+	public ResponseEntity<Page<Pedido>> listarPagina(
+			@RequestParam(value = "page", defaultValue = "0") Integer page, 
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
+			@RequestParam(value = "ordeBy", defaultValue = "instante") String orderBy, 
+			@RequestParam(value = "direction", defaultValue = "DESC") String direction) {
+		Page<Pedido> pedidos = pedidoService.listarPorPagina(page, linesPerPage, orderBy, direction);
+		return ResponseEntity.ok().body(pedidos);
 	}
 }
--- src/main/java/com/codar/vendas/services/PedidoService.java
+++ src/main/java/com/codar/vendas/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.codar.vendas.domain.Cliente;
 import com.codar.vendas.domain.ItemPedido;
 import com.codar.vendas.domain.PagamentoComBoleto;
 import com.codar.vendas.domain.Pedido;
@@ -16,6 +20,8 @@ import com.codar.vendas.repository.ItemPedidoRepository;
 import com.codar.vendas.repository.PagamentoRepository;
 import com.codar.vendas.repository.PedidoRepository;
 import com.codar.vendas.repository.ProdutoRepository;
+import com.codar.vendas.security.UserSS;
+import com.codar.vendas.services.exceptions.AuthorizationException;
 import com.codar.vendas.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -71,5 +77,15 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(pedido.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(pedido);
 		return pedido;
+	}
+	
+	public Page<Pedido> listarPorPagina(Integer page, Integer linesPerPage, String orderBy, String direction){
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso Negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteRepository.findById(user.getId()).get();
+		return pedidoRepository.findByCliente(cliente, pageRequest);
 	}
 }
